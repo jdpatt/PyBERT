Dataclass classmethods don't work as I expect.  Remove my fix to moving average, I think David was padded this intentionally.  Add basic content for the GUI.  Need to implement callback or publish/subscribe to get everything from simulation() to auto populate and update the GUI.

pybert/jitter.py
@@ -1,5 +1,4 @@
 """Jitter calculation and container for the system."""
-from dataclasses import dataclass
 from logging import getLogger
 
 import numpy as np
@@ -14,24 +13,41 @@ from scipy.stats import norm
 log = getLogger("pybert.jitter")
 
 
-@dataclass
 class Jitter:
     """docstring for Jitter"""
 
-    jitter = 0.0
-    t_jitter = 0.0
-    isi = 0.0
-    dcd = 0.0
-    pj = 0.0
-    rj = 0.0
-    tie_ind = np.array([])
-    thresh = np.array([])
-    jitter_spectrum = np.array([])
-    tie_ind_spectrum = np.array([])
-    spectrum_freqs = np.array([])
-    hist = np.array([])
-    hist_synth = np.array([])
-    bin_centers = np.array([])
+    def __init__(
+        self,
+        jitter,
+        t_jitter,
+        isi,
+        dcd,
+        pj,
+        rj,
+        tie_ind,
+        thresh,
+        jitter_spectrum,
+        tie_ind_spectrum,
+        spectrum_freqs,
+        hist,
+        hist_synth,
+        bin_centers,
+    ):
+
+        self.jitter = jitter
+        self.t_jitter = t_jitter
+        self.isi = isi
+        self.dcd = dcd
+        self.pj = pj
+        self.rj = rj
+        self.tie_ind = tie_ind
+        self.thresh = thresh
+        self.jitter_spectrum = jitter_spectrum
+        self.tie_ind_spectrum = tie_ind_spectrum
+        self.spectrum_freqs = spectrum_freqs
+        self.hist = hist
+        self.hist_synth = hist_synth
+        self.bin_centers = bin_centers
 
     @classmethod
     def calc_jitter(

pybert/utility.py
@@ -6,8 +6,9 @@ Original date:   September 27, 2014 (Copied from pybert_cntrl.py.)
 
 Copyright (c) 2014 David Banas; all rights reserved World wide.
 """
-import matplotlib # isort:skip
-matplotlib.use("Qt5Agg") # isort:skip
+import matplotlib  # isort:skip
+
+matplotlib.use("Qt5Agg")  # isort:skip
 
 import re
 
@@ -22,8 +23,6 @@ from typing import List, Sequence
 import numpy as np
 import skrf as rf
 from scipy.signal import freqs, get_window, invres
-
-
 
 log = getLogger("pybert.utility")
 
@@ -84,7 +83,12 @@ def moving_average(a, n=3):
 
     ret = np.cumsum(a, dtype=np.float)
     ret[n:] = ret[n:] - ret[:-n]
-    return ret[n - 1 :] / n
+    # return ret[n - 1 :] / n
+    # TODO: This returns an array that is shorter than the orginal which causes pybert to crash.
+    # Looking at line 203 in jitter.py, he may have intentionally padded
+    # this just didn't note here that its intentionally padded.
+    # Error: operands could not be broadcast together with shapes (8000,) (7201,)
+    return np.insert(ret[n - 1 :], 0, ret[n - 1] * np.ones(n - 1)) / n
 
 
 def find_crossing_times(