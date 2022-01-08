c42ea7663ac3e5bd6840bfbe28d442244e68ee93
David Patterson <14203439+jdpatt@users.noreply.github.com>
2022-01-08 10:11:46 -0500

    Update the plot data for the results to be valid.

--- pybert/cli.py
+++ pybert/cli.py
@@ -45,7 +45,7 @@ def simulate(config, verbose):
     setup_logging(verbose)
     pybert = PyBERT(run_simulation=False, gui=False)
     pybert.load_configuration(config)
-    pybert.simulate(initial_run=True, update_plots=False)
+    pybert.simulate(initial_run=True, update_plots=True)
     pybert.save_results(Path(config).with_suffix(".pybert_data"))
 
 
