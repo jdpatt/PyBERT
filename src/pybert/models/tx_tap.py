32198ab090aba917192300eb8f86188889cfdf25
David Patterson <14203439+jdpatt@users.noreply.github.com>
2025-04-26 13:39:07 -0400

    feat: convert TxTapTuner to dataclass

--- src/pybert/models/tx_tap.py
+++ src/pybert/models/tx_tap.py
@@ -1,34 +1,17 @@
 "Tx FFE tap weight tuner, used by the optimizer."
 
-from traits.api import Bool, Float, HasTraits, Int, String
+from dataclasses import dataclass
 
 
-# pylint: disable=too-many-instance-attributes,too-few-public-methods
-class TxTapTuner(HasTraits):
+@dataclass
+class TxTapTuner:
     """Object used to populate the rows of the Tx FFE tap tuning table."""
 
-    name = String("(noname)")
-    pos = Int(0)  # negative = pre-cursor; positive = post-cursor
-    enabled = Bool(False)
-    min_val = Float(-0.1)
-    max_val = Float(0.1)
-    step = Float(0.01)
-    value = Float(0.0)
-    steps = Int(0)  # Non-zero means we want to sweep it.
-
-    # pylint: disable=too-many-arguments,too-many-positional-arguments
-    def __init__(self, name="(noname)", pos=0, enabled=False, min_val=-0.1, max_val=0.1, step=0.01, value=0.0, steps=0):
-        """Allows user to define properties, at instantiation."""
-
-        # Super-class initialization is ABSOLUTELY NECESSARY, in order
-        # to get all the Traits/UI machinery setup correctly.
-        super().__init__()
-
-        self.name = name
-        self.pos = pos
-        self.enabled = enabled
-        self.min_val = min_val
-        self.max_val = max_val
-        self.step = step
-        self.value = value
-        self.steps = steps
+    name: str = "(noname)"
+    pos: int = 0  # negative = pre-cursor; positive = post-cursor
+    enabled: bool = False
+    min_val: float = -0.1
+    max_val: float = 0.1
+    step: float = 0.01
+    value: float = 0.0
+    steps: int = 0  # Non-zero means we want to sweep it.
